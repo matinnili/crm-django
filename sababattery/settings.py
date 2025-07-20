d1f27b02eb5af8b9d11ad25145029b257dcf529d
Your Name <you@example.com>
2025-07-20 11:28:37 +0000

    add unfold

--- sababattery/settings.py
+++ sababattery/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crm',
+    'unfold'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+UNFOLD = {
+    "SITE_TITLE": "My Admin"
+    
+}
